d46ac510ce7284ebc161da44be4c8ea840a3cdec
Nikita Bylym <nikitabylym0@gmail.com>
2021-12-22 01:58:30 +0200

    Add files via upload

---
+++ DepositLib/IDepositDatacs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    public interface IDepositData
+    {
+        /// <summary>
+        /// Today Date
+        /// </summary>
+        DateTime DateAgreement { get; set; }
+        /// <summary>
+        /// Term Data
+        /// </summary>
+        DateTime DateCalculation { get; set; }
+        /// <summary>
+        /// Type Capitalization
+        /// </summary>
+        string TypeCapital { get; set; }
+        /// <summary>
+        /// Sum Deposit
+        /// </summary>
+        decimal DepositSum { get; set; }
+        /// <summary>
+        /// Prosent Deposit
+        /// </summary>
+        double ProsentDepositR { get; set; }
+
+        decimal SumProsentN { get; set; }
+        /// <summary>
+        /// Replenishment Deposit
+        /// </summary>
+        decimal ReplenishmentDeposit { get; set; }
+    }
+}
